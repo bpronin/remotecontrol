b9cf2fd3f896df003758e36b728f7b7c30b6e7b7
Boris Pronin <boris.i.pronin@gmail.com>
2015-08-08 23:05:30 +0300

    Initial commit

---
+++ app/src/main/java/com/bo/android/remotecontrol/MainActivityFragment.java
@@ -0,0 +1,91 @@
+package com.bo.android.remotecontrol;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private TextView selectedTextView;
+    private TextView workingTextView;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        selectedTextView = (TextView) view.findViewById(R.id.fragment_remote_control_selectedTextView);
+        workingTextView = (TextView) view.findViewById(R.id.fragment_remote_control_workingTextView);
+
+        View.OnClickListener numberButtonListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                TextView textView = (TextView) v;
+                String working = workingTextView.getText().toString();
+                String text = textView.getText().toString();
+                if (working.equals("0")) {
+                    workingTextView.setText(text);
+                } else {
+                    workingTextView.setText(working + text);
+                }
+            }
+        };
+
+        TableLayout tableLayout = (TableLayout) view.findViewById(R.id.fragment_remote_control_tableLayout);
+        int number = 1;
+        for (int i = 2; i < tableLayout.getChildCount() - 1; i++) {
+            TableRow row = (TableRow) tableLayout.getChildAt(i);
+            for (int j = 0; j < row.getChildCount(); j++) {
+                Button button = (Button) row.getChildAt(j);
+                button.setText("" + number);
+                button.setOnClickListener(numberButtonListener);
+                number++;
+            }
+        }
+
+        TableRow bottomRow = (TableRow) tableLayout.getChildAt(tableLayout.getChildCount() - 1);
+
+        Button deleteButton = (Button) bottomRow.getChildAt(0);
+        deleteButton.setText("Delete");
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                workingTextView.setText("0");
+            }
+        });
+
+        Button zeroButton = (Button) bottomRow.getChildAt(1);
+        zeroButton.setText("0");
+        zeroButton.setOnClickListener(numberButtonListener);
+
+        Button enterButton = (Button) bottomRow.getChildAt(2);
+        enterButton.setText("Enter");
+        enterButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                CharSequence working = workingTextView.getText();
+                if (working.length() > 0) {
+                    selectedTextView.setText(working);
+                }
+                workingTextView.setText("0");
+            }
+        });
+
+        return view;
+    }
+
+}
